b0a954c56271806e4cf83730abb1e07802661800
Tony-Tor <Tony.Tor2016@yandex.ru>
2023-07-29 16:16:31 +0400

    Initial commit

---
+++ src/main/java/com/example/gsws_ap_b_ch7_p1/GswsApBCh7P1Application.java
@@ -0,0 +1,13 @@
+package com.example.gsws_ap_b_ch7_p1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GswsApBCh7P1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GswsApBCh7P1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gsws_ap_b_ch7_p1/GswsApBCh7P1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gsws_ap_b_ch7_p1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GswsApBCh7P1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
